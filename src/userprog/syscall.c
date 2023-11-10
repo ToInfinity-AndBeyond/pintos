4671bfa894ca6cce8314a2939f5c4378afeb9d7c
minseok_chey <techalt7@gmail.com>
2023-11-10 10:58:56 +0000

    Modified syscall.c to implement syscall functionality.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -10,12 +10,17 @@
 #include "threads/malloc.h"
 #include "threads/vaddr.h"
 
-// Many functions treat pid_t and tid_t the same (UTA confirmation this is fine)
-// Many functions treat struct file * and fd the same
+static void syscall_handler (struct intr_frame *f);
+void syscall_init(void);
 
-#define MAX_ARGS 7
+void check_ptr(void *ptr);
 
-static void syscall_handler (struct intr_frame *);
+void halt(void);
+void exit(uint32_t *esp);
+int exec(uint32_t *esp);
+int wait(uint32_t *esp);
+int write(uint32_t *esp);
+int read(uint32_t *esp);
 
 void
 syscall_init (void) 
@@ -23,67 +28,48 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void
-syscall_get_args(int no_args, int *args, struct intr_frame *f) {
-  for (int i = 0; i < no_args; i++) {
-      args[i] = *(uint32_t *) (f->esp + (i + 1) * 4);
-  }
-}
 
 static void
 syscall_handler (struct intr_frame *f) {
-  uint32_t args[MAX_ARGS];
-  
-  check_ptr(f);
   int *syscall_args = {0, 1, 1, 1, 2, 1, 1, 1, 3, 3, 2, 1, 1};
   int syscall_no = *(int *) f->esp;
-  int syscall_ret;
-  syscall_get_args(syscall_args[syscall_no], args, f);
+  uint32_t *esp = (uint32_t *)f->esp;
+  check_ptr(esp);
 
   switch (syscall_no) {
     case SYS_HALT: 
       halt();
       break;
     case SYS_EXIT:
-      exit((int) args[0]);
+      exit(esp);
       break;
     case SYS_EXEC:
-      syscall_ret = exec((const char *) args[0]);
-      break;
-    case SYS_WAIT:
-      syscall_ret = wait((pid_t) args[0]);
+      f->eax = exec(esp);
       break;
     case SYS_CREATE:
-      syscall_ret = create((const char *) args[0], (unsigned) args[1]);
       break;
     case SYS_REMOVE:
-      syscall_ret = remove((const char *) args[0]);
       break;
     case SYS_OPEN:
-      syscall_ret = open((const char *) args[0]);
       break;
     case SYS_FILESIZE:
-      syscall_ret = filesize((int) args[0]);
+      break;
+    case SYS_WAIT:
+      f->eax = wait(esp);
       break;
     case SYS_READ:
-      syscall_ret = read((int) args[0], (void *) args[1], (unsigned) args[2]);
+      f->eax = read(esp);
       break;
     case SYS_WRITE:
-      syscall_ret = write((int) args[0], (void *) args[1], (unsigned) args[2]);
+      f->eax = write(esp);
       break;
     case SYS_SEEK:
-      seek((int) args[0], (unsigned) args[1]);
       break;
     case SYS_TELL:
-      syscall_ret = tell((int) args[0]);
       break;
     case SYS_CLOSE:
-      close((int) args[0]);
       break;
-    }
-
-  printf ("system call!\n");
-  thread_exit ();
+  }
 }
 
 void
@@ -105,89 +91,49 @@ void halt(void)
   shutdown_power_off();
 }
 
-void exit(int status)
+void exit(uint32_t *esp)
 {
-  struct thread *t = thread_current();
-  printf("%s: exit(%d)\n", t->name, status);
+  int status = (int)esp[1];
+  printf("%s: exit(%d)\n", thread_name(), status);
+  thread_current()->exit_status = status;
   thread_exit();  
 }
 
-// TODO: Need a semaphore here for ordering
-pid_t exec(const char *cmd_line)
+int wait(uint32_t *esp)
 {
-  return process_execute(cmd_line);
+  return process_wait((int)esp[1]);
 }
 
-// Uses int process_wait(tid_t child_tid)
-int wait(pid_t pid)
+int exec(uint32_t *esp)
 {
-  struct thread *t = thread_current();
-  struct thread *relevant_child;
-  bool found = false;
+  return process_execute((char *)esp[1]);
+}
 
-  struct list_elem *e;
-  for (e = list_begin(&t->children_list); e != list_end(&t->children_list); e = list_next(e))
-  {
-    struct thread *child = list_entry(e, struct thread, child_elem);
-    if (child->tid == pid)
-    {
-      relevant_child = child;
-      found = true;
+int write(uint32_t *esp)
+{
+  int fd = (int)esp[1];
+  void *buffer = (void *)esp[2];
+  unsigned size = (int)esp[3];
+  if (fd == 1) {
+    putbuf(buffer, size);
+    return size;
+  }
+  return -1;
+}
+
+int read(uint32_t *esp)
+{
+  int fd = (int)esp[1];
+  void *buffer = (void *)esp[2];
+  unsigned size = (int)esp[3];
+  if (fd == 0) {
+    int i;
+    for (i = 0; input_getc() || i <= size; ++i) {
+
     }
+    return i;
   }
-
-  if (!found || relevant_child->parent_is_waiting)
-  {
-    return -1;
-  }
-
-  relevant_child->parent_is_waiting = true;
-
-  // Seems as if I could do this at the beginning so this is incorrect
-  return process_wait(relevant_child->tid);
+  return -1;
 }
 
-bool create(const char *file, unsigned initial_size)
-{
-  return filesys_create(file, initial_size);
-}
 
-bool remove(const char *file)
-{
-  return filesys_remove(file);
-}
-
-int open(const char *file)
-{
-  return filesys_open(file);
-}
-
-int filesize(int fd)
-{
-  return file_length(fd);
-}
-
-int read(int fd, void *buffer, unsigned size)
-{
-  return file_read(fd, buffer, size);
-}
-
-int write(int fd, const void *buffer, unsigned size)
-{
-  return file_write(fd, buffer, size);
-}
-
-void seek(int fd, unsigned position)
-{
-  file_seek(fd, position);
-}
-
-unsigned tell(int fd)
-{
-  return file_tell(fd);
-}
-
-void close(int fd)
-{
-  file_close(fd);
-}
