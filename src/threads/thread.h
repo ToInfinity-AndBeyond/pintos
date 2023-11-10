cebb16f48b858f355e95c87be8ab4b826d449e67
minseok_chey <techalt7@gmail.com>
2023-11-10 11:04:56 +0000

    Added member variable for struct thread, that is exclusively used for USERPROG.

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -109,6 +109,11 @@ struct thread
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
+    struct list children_list;
+    struct list_elem child_elem;
+    int exit_status;
+    struct semaphore child_lock;
+    struct semaphore memory_lock;
 #endif
 
     /* Owned by thread.c. */
