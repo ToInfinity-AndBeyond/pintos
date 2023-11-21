4400dda13250ca8ec31e36e7ff94add2b8a96088
Basit Khan <bmk22@ray18.doc.ic.ac.uk>
2023-11-21 14:19:59 +0000

    Revert frame table back to hash upon discussion

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -7,25 +7,25 @@ struct frame_table_entry {
   int frame_no;
   void *page;
   int64_t ticks;
-  struct list_elem elem;
+  struct hash_elem helem;
 };
 
-struct list frame_table;
+struct hash frame_table;
 
-unsigned frame_hash_func(const struct list_elem *element, void *aux)
+unsigned frame_hash_func(const struct hash_elem *element, void *aux)
 {
-  return hash_int(list_entry(element, struct frame_table_entry, elem)->frame_no);
+  return hash_int(hash_entry(element, struct frame_table_entry, helem)->frame_no);
 }
 
-bool frame_less_func(const struct list_elem *a, const struct list_elem *b, void *aux)
+bool frame_less_func(const struct hash_elem *a, const struct hash_elem *b, void *aux)
 {
-  struct frame_table_entry *entrya = list_entry(a, struct frame_table_entry, elem);
-  struct frame_table_entry *entryb = list_entry(b, struct frame_table_entry, elem);
+  struct frame_table_entry *entrya = hash_entry(a, struct frame_table_entry, helem);
+  struct frame_table_entry *entryb = hash_entry(b, struct frame_table_entry, helem);
 
   return entrya->frame_no < entryb->frame_no;
 }
 
 void frame_init(void)
 {
-  list_init(&frame_table);
+  hash_init(&frame_table, &frame_hash_func, &frame_less_func, NULL);
 }
