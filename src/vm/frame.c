4fe3e275fbbea78ffbf90ca8cc8865b0a491114d
Basit Khan <bmk22@ray18.doc.ic.ac.uk>
2023-11-24 13:10:54 +0000

    Add pid to frame_table_entry, change frame_add to take pid, add function to remove frames belonging to process

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -5,6 +5,8 @@
 
 static struct hash frame_table;
 
+static struct lock frame_no_lock;
+
 unsigned frame_hash_func(const struct hash_elem *element, void *aux UNUSED)
 {
   return hash_int(hash_entry(element, struct frame_table_entry, helem)->frame_no);
@@ -21,13 +23,27 @@ bool frame_less_func(const struct hash_elem *a, const struct hash_elem *b, void 
 // Frees the frame_table_entry which e is embedded within
 void frame_free_func(struct hash_elem *e, void *aux UNUSED)
 {
-  free(hash_entry(e, struct frame_table_entry, helem));
+  struct frame_table_entry *entry = hash_entry(e, struct frame_table_entry, helem);
+  palloc_free_page(entry->page);
+  free(entry);
+}
+
+static int allocate_frame_no(void)
+{
+  static next_frame_no = 1;
+
+  lock_acquire(&frame_no_lock);
+  int frame_no = next_frame_no++;
+  lock_release(&frame_no_lock);
+
+  return frame_no;
 }
 
 void frame_init(void)
 {
   bool success = hash_init(&frame_table, frame_hash_func, frame_less_func, NULL);
   if (!success) PANIC("Frame table could not be allocated");
+  lock_init(&frame_no_lock);
 }
 
 // Returns the frame_table_entry * with the corresponding frame_no
@@ -40,16 +56,20 @@ struct frame_table_entry *frame_lookup(int frame_no)
 }
 
 // If the frame_no already exists, removes and frees the frame_table_entry
-void frame_add(int frame_no, void *page)
+void frame_add(pid_t pid)
 {
   struct frame_table_entry *frame = malloc(sizeof(struct frame_table_entry));
   if (frame == NULL) PANIC("Frame entry could not be initialised");
 
   *frame = (struct frame_table_entry) {
-    .frame_no = frame_no,
-    .page = page,
+    .frame_no = allocate_frame_no(),
+    .pid = pid,
     .ticks = timer_ticks()
   };
+
+  void *page = palloc_get_page(PAL_USER);
+  if (page == NULL) PANIC("Run out of frames");
+  frame->page = page;
 
   struct hash_elem *e = hash_replace(&frame_table, &frame->helem);
   if (e != NULL) frame_free_func(e, NULL);
@@ -63,6 +83,21 @@ void frame_remove(int frame_no)
   struct hash_elem *e = hash_delete(&frame_table, &f.helem);
 
   if (e != NULL) frame_free_func(e, NULL);
+}
+
+void frame_remove_process(pid_t pid)
+{
+  struct hash_iterator i;
+
+  hash_first(&i, &frame_table);
+  while (hash_next(&i))
+  {
+    struct frame_table_entry *entry = hash_entry(hash_cur(&i), struct frame_table_entry, helem);
+    if (entry->pid == pid)
+    {
+      frame_remove(entry->frame_no);
+    }
+  }
 }
 
 void frame_clear(void)
--- src/vm/frame.h
+++ src/vm/frame.h
@@ -1,7 +1,9 @@
 #include <hash.h>
+#include "lib/user/syscall.h"
 
 struct frame_table_entry {
   int frame_no;
+  pid_t pid;
   void *page;
   int64_t ticks;
   struct hash_elem helem;
@@ -13,7 +15,8 @@ extern void frame_free_func(struct hash_elem *e, void *aux);
 
 extern void frame_init(void);
 extern struct frame_table_entry *frame_lookup(int frame_no);
-extern void frame_add(int frame_no, void *page);
+extern void frame_add(pid_t pid);
 extern void frame_remove(int frame_no);
+extern void frame_remove_process(pid_t pid);
 extern void frame_clear(void);
 extern void frame_destroy(void);
